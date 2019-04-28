aa312c6d24bdcb9a08eeefed03840536ffd534fb
beduffy <benfduffy@gmail.com>
2019-04-28 23:24:06 +0100

    Updated urdf of vector and moved wheel joints into correct place (changed a million origins, removed poses). Added head.mtl and texture uv map (because pybullet can only use 1 material per obj...)

--- load_model.py
+++ load_model.py
@@ -10,11 +10,13 @@ p.resetSimulation()
 p.setGravity(0, 0, -10)
 p.loadURDF("plane.urdf")  # todo why gap between ground at start? Probably to do with origin, scale and collision properties
 
-vector_start_position = [0, 0, 10]
+vector_start_position = [0, 0, 2]
 # vector_model = p.loadSDF('model.sdf', globalScaling=10.0)
+vector_model = p.loadURDF('vector_from_sdf.urdf', globalScaling=5.0, basePosition=vector_start_position,
 # vector_model = p.loadURDF('vector.urdf', globalScaling=5.0,
-#                           flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL
-#                                                        )
+                          flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL
+                                                       )
+
 
 
 
@@ -25,15 +27,18 @@ quat_orientation = p.getQuaternionFromEuler([1, 0, 0])
 # teacup_model = p.loadSDF('teacup_model.sdf')
 # teacup_model = p.loadSDF('teacup_model_mesh_collision.sdf')
 # teacup_model = p.loadURDF('TeaCup.urdf', basePosition=pos, baseOrientation=quat_orientation, flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL)
-teacup_model = p.loadURDF('coffee_cup.urdf', basePosition=pos,
-                          baseOrientation=quat_orientation, flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL,
-                          globalScaling=0.1)
-
+# teacup_model = p.loadURDF('coffee_cup.urdf', basePosition=pos,
+#                           baseOrientation=quat_orientation, flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL,
+#                           globalScaling=0.1)
+#
 # print(vector_model)
+
+time.sleep(2)
 
 for i in range(10000):
    p.stepSimulation()
    time.sleep(1/ 240)
-   #p.changeDynamics(vector_model[0], -1)
+   # time.sleep(1)
+   # p.changeDynamics(vector_model[0], -1)
 
-time.sleep(20)
+# time.sleep(20)
