fcf65e2bafe74cfa4e9511826226f0b37a558160
beduffy <benfduffy@gmail.com>
2019-06-14 20:02:53 +0100

    Added kitchen.urdf, kuka_with_gripper2.sdf (modified with wheels and a base but not working), load_model.py loading kitchen_urdf with tea cup

--- load_model.py
+++ load_model.py
@@ -10,13 +10,15 @@ if (cid < 0):
 p.resetSimulation()
 p.setGravity(0, 0, -10)
 p.loadURDF("plane.urdf")  # todo why gap between ground at start? Probably to do with origin, scale and collision properties
+p.loadURDF("kitchen.urdf")
+# p.loadURDF("IAI_kitchen.urdf")
 
 vector_start_position = [0, 0, 2]
 # vector_model = p.loadSDF('model.sdf', globalScaling=10.0)
-vector_model = p.loadURDF('vector_from_sdf.urdf', globalScaling=5.0, basePosition=vector_start_position,
+# vector_model = p.loadURDF('vector_from_sdf.urdf', globalScaling=5.0, basePosition=vector_start_position,
 # vector_model = p.loadURDF('vector.urdf', globalScaling=5.0,
 #                           flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL
-                                                       )
+#                                                        )
 
 # for i in range(p.getNumJoints(vector_model)):
 #     print(p.getJointInfo(vector_model, i))
@@ -28,11 +30,12 @@ vector_model = p.loadURDF('vector_from_sdf.urdf', globalScaling=5.0, basePositio
 
 # <scale>0.0280866 0.0281665 0.0197164/scale>
 # <scale>.01 .01 .01</scale>
-pos = [0, 0, 3]
-quat_orientation = p.getQuaternionFromEuler([1, 0, 0])
+pos = [3.3, 2, 1]
+quat_orientation = p.getQuaternionFromEuler([0, 0, 3.14 / 2])
 # teacup_model = p.loadSDF('teacup_model.sdf')
 # teacup_model = p.loadSDF('teacup_model_mesh_collision.sdf', globalScaling=5.0)
-# teacup_model = p.loadURDF('TeaCup.urdf', basePosition=pos, baseOrientation=quat_orientation, flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL)
+teacup_model = p.loadURDF('TeaCup.urdf', basePosition=pos, baseOrientation=quat_orientation,
+                          flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL, globalScaling=0.1)
 # teacup_model = p.loadURDF('TeaCup.urdf', basePosition=pos, flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL)
 # teacup_model = p.loadURDF('coffee_cup.urdf', basePosition=pos,
 #                           baseOrientation=quat_orientation, flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL,
