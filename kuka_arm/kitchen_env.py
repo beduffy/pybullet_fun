5dc0bbc55c54b8f1e71ac9a1b8fb00131c8c1202
beduffy <benfduffy@gmail.com>
2019-07-04 17:46:06 +0100

    Put all objects into config list to loop through, stored unique names and created dict. subplots, grid-class datastructure list of lists. legend and grid colours not matched, debugging code. Raised wall height to door height.

--- kuka_arm/kitchen_env.py
+++ kuka_arm/kitchen_env.py
@@ -1,10 +1,13 @@
 import math
 import time
+from collections import Counter
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
 from matplotlib import colors
-
+from matplotlib.font_manager import FontProperties
 import pybullet as p
 
 start_time = time.time()
@@ -17,42 +20,55 @@ p.resetSimulation()
 p.setGravity(0, 0, -9.8)
 p.loadURDF("plane.urdf")
 
+default_quat_orientation = p.getQuaternionFromEuler([0, 0, 0])
 quat_orientation = p.getQuaternionFromEuler([0, 0, 3.14 / 2])
 
-# Dynamic objects
-beer_bottle = p.loadURDF("beer_bottle.urdf", basePosition=[3.4, 1.5, 1.1])
-bowl = p.loadURDF("bowl.urdf", basePosition=[3.4, 1.5, 1.1])
-plate = p.loadURDF("plate.urdf", basePosition=[3.4, 1, 1.1])
-glass = p.loadURDF("glass.urdf", basePosition=[3.5, 1.3, 1.1])
-knife = p.loadURDF("knife.urdf", basePosition=[3.6, 1.7, 1.1], globalScaling=0.01)
-spoon = p.loadURDF("spoon.urdf", basePosition=[3.5, 1.2, 1.1], globalScaling=0.01)
-fork = p.loadURDF("fork.urdf", basePosition=[3.4, 1.65, 1.1], globalScaling=1.0)
-tissue_box = p.loadURDF("tissue_box.urdf", basePosition=[3.5, 0.7, 1.1], globalScaling=0.016)
-tissue_box2 = p.loadURDF("tissue_box.urdf", basePosition=[3.5, 6, 1.5], globalScaling=0.016)
-banana = p.loadURDF("banana.urdf", basePosition=[3.6, 2.3, 1.1], globalScaling=1.0)
-crisps_chips = p.loadURDF("crisps_chips.urdf", basePosition=[3.5, 2.4, 1.1], globalScaling=0.01)
-crisps_chips2 = p.loadURDF("crisps_chips.urdf", basePosition=[3.45, 6.35, 1.5], globalScaling=0.01)
-teacup_model1 = p.loadURDF('TeaCup.urdf', basePosition=[3.4, 2, 1.1], baseOrientation=quat_orientation,
-                          flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL, globalScaling=0.1)
-teacup_model2 = p.loadURDF('TeaCup.urdf', basePosition=[3.6, 5.8, 1.55], baseOrientation=quat_orientation,
-                          flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL, globalScaling=0.1)
-teacup_model3 = p.loadURDF('TeaCup.urdf', basePosition=[1.5, 5.8, 1.5], baseOrientation=quat_orientation,
-                          flags=p.URDF_USE_MATERIAL_COLORS_FROM_MTL, globalScaling=0.1)
+all_objects = [
+    {'urdf_fn': 'beer_bottle.urdf', 'basePosition': [3.4, 1.5, 1.1], 'dynamic': True},
+    {'urdf_fn': 'bowl.urdf', 'basePosition': [3.4, 1.5, 1.1], 'dynamic': True},
+    {'urdf_fn': 'plate.urdf', 'basePosition': [3.4, 1, 1.1], 'dynamic': True},
+    {'urdf_fn': 'glass.urdf', 'basePosition': [3.5, 1.3, 1.1], 'dynamic': True},
+    {'urdf_fn': 'knife.urdf', 'basePosition': [3.6, 1.7, 1.1], 'dynamic': True, 'globalScaling': 0.01},
+    {'urdf_fn': 'spoon.urdf', 'basePosition': [3.5, 1.2, 1.1], 'dynamic': True, 'globalScaling': 0.01},
+    {'urdf_fn': 'fork.urdf', 'basePosition': [3.4, 1.65, 1.1], 'dynamic': True, 'globalScaling': 1.0},
+    {'urdf_fn': 'tissue_box.urdf', 'basePosition': [3.5, 0.7, 1.1], 'dynamic': True, 'globalScaling': 0.016},
+    {'urdf_fn': 'tissue_box.urdf', 'basePosition': [3.5, 6, 1.5], 'dynamic': True, 'globalScaling': 0.016},
+    {'urdf_fn': 'banana.urdf', 'basePosition': [3.6, 2.3, 1.1], 'dynamic': True},
+    {'urdf_fn': 'crisps_chips.urdf', 'basePosition': [3.5, 2.4, 1.1], 'dynamic': True, 'globalScaling': 0.01},
+    {'urdf_fn': 'crisps_chips.urdf', 'basePosition': [3.45, 6.35, 1.5], 'dynamic': True, 'globalScaling': 0.01},
+    {'urdf_fn': 'TeaCup.urdf', 'basePosition': [3.4, 2, 1.1], 'dynamic': True, 'baseOrientation': quat_orientation, 'globalScaling': 0.06, 'flags': p.URDF_USE_MATERIAL_COLORS_FROM_MTL},
+    {'urdf_fn': 'TeaCup.urdf', 'basePosition': [3.6, 5.8, 1.55], 'dynamic': True, 'baseOrientation': quat_orientation, 'globalScaling': 0.06, 'flags': p.URDF_USE_MATERIAL_COLORS_FROM_MTL},
+    {'urdf_fn': 'TeaCup.urdf', 'basePosition': [1.5, 5.8, 1.5], 'dynamic': True, 'baseOrientation': quat_orientation, 'globalScaling': 0.06, 'flags': p.URDF_USE_MATERIAL_COLORS_FROM_MTL},
+    {'urdf_fn': 'sofa.urdf', 'basePosition': [0.5, 6, 1], 'dynamic': True, 'baseOrientation': quat_orientation},
+    {'urdf_fn': 'table.urdf', 'basePosition': [1.5, 6, 1], 'dynamic': True, 'baseOrientation': quat_orientation},
+    {'urdf_fn': 'table.urdf', 'basePosition': [3.5, 6, 1], 'dynamic': True, 'baseOrientation': quat_orientation, 'globalScaling': 1.5},
+    {'urdf_fn': 'trash_can.urdf', 'basePosition': [3.5, 9, 1], 'dynamic': True, 'baseOrientation': quat_orientation},
+    {'urdf_fn': 'trash_can.urdf', 'basePosition': [3.5, 4, 1], 'dynamic': True, 'baseOrientation': quat_orientation},
+    {'urdf_fn': 'bed.urdf', 'basePosition': [1.1, 7.8, 1], 'dynamic': True, 'baseOrientation': quat_orientation, 'globalScaling': 1.7},
+    {'urdf_fn': 'nordic_floor_lamp_obj.urdf', 'basePosition': [1.5, 9, 1.3], 'dynamic': True, 'baseOrientation': quat_orientation, 'globalScaling': 0.01},
+    {'urdf_fn': 'kitchen_walls.urdf', 'dynamic': False},  # todo some link segmentation is black because https://github.com/bulletphysics/bullet3/issues/1631
+    {'urdf_fn': 'kitchen.urdf', 'basePosition': [0.1, 0.15, 0.1], 'dynamic': False}
+]
 
-# Mostly static
-sofa = p.loadURDF("sofa.urdf", basePosition=[0.5, 6, 1], baseOrientation=quat_orientation)
-table = p.loadURDF("table.urdf", basePosition=[1.5, 6, 1], baseOrientation=quat_orientation)
-table2 = p.loadURDF("table.urdf", basePosition=[3.5, 6, 1], globalScaling=1.5,
-                   baseOrientation=quat_orientation)
-trash_can1 = p.loadURDF("trash_can.urdf", basePosition=[3.5, 9, 1], baseOrientation=quat_orientation)
-trash_can2 = p.loadURDF("trash_can.urdf", basePosition=[3.5, 4, 1], baseOrientation=quat_orientation)
-bed = p.loadURDF("bed.urdf", basePosition=[1.1, 7.8, 1], globalScaling=1.7, baseOrientation=quat_orientation)
-lamp = p.loadURDF("nordic_floor_lamp_obj.urdf", basePosition=[1.5, 9, 1.3], globalScaling=0.01,
-                 baseOrientation=quat_orientation)
+all_obj_names_counter = Counter()
+for idx, object_spec in enumerate(all_objects):
+    print('Loading urdf object with specification: {}'.format(object_spec))
+    obj_id = p.loadURDF(object_spec['urdf_fn'], basePosition=object_spec.get('basePosition', [0, 0, 0]),
+                        globalScaling=object_spec.get('globalScaling', 1.0),
+                        baseOrientation=object_spec.get('baseOrientation', default_quat_orientation),
+                        flags=object_spec.get('flags', 0)
+                        )
+    all_objects[idx]['obj_id'] = obj_id
+    canonical_name = object_spec['urdf_fn'][:-5]
+    all_obj_names_counter.update([canonical_name])
+    all_objects[idx]['obj_name'] = '{}_{}'.format(canonical_name,
+                                                  all_obj_names_counter['canonical_name'])
 
 
-kitchen_walls_and_floor = p.loadURDF("kitchen_walls.urdf")
-kitchen = p.loadURDF("kitchen.urdf", basePosition=[0.2, 0.2, 0.1])
+all_obj_ids = [obj['obj_id'] for obj in all_objects]
+obj_id_to_obj_name = {obj['obj_id']: obj['obj_name'] for obj in all_objects}
+obj_name_to_obj_id = {obj['obj_name']: obj['obj_id'] for obj in all_objects}
+print(obj_id_to_obj_name)
 
 # todo attempt to make kitchen static while still keeping cupboards openable
 # kitchen = p.loadURDF("kitchen.urdf")
@@ -97,8 +113,6 @@ def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=F
     """
     from matplotlib.colors import LinearSegmentedColormap
     import colorsys
-    import numpy as np
-
 
     if type not in ('bright', 'soft'):
         print ('Please choose "bright" or "soft" for type')
@@ -209,10 +223,11 @@ def create_point_cloud_and_occupancy_grid():
     cornersY = [0, 0, height, height]
     corners3D = []
 
-    imgW = int(width / 10)
+    imgW = int(width / 10)  # todo why do we do this? try not doing this. Is this the gap in ray tracing?
     imgH = int(height / 10)
 
     img = p.getCameraImage(imgW, imgH, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    # img = p.getCameraImage(imgW, imgH)
     rgbBuffer = img[2]
     depthBuffer = img[3]
     segBuffer = img[4]
@@ -267,7 +282,7 @@ def create_point_cloud_and_occupancy_grid():
             newTo = (depth / l) * vec + rf
 
             # todo we aren't shooting a ray out of each pixel obviously? So what are we doing? Floating point approximation on mouseX?
-            if math.sqrt(newTo[0] ** 2 + newTo[1] ** 2) < 30:  # magic number for now
+            if math.sqrt(newTo[0] ** 2 + newTo[1] ** 2) < 30:  # magic number for now  # todo could put floor check here
                 all_ball_locations.append(newTo)
                 all_ball_seg_objs.append(segBuffer[h, w])
 
@@ -295,17 +310,12 @@ def create_point_cloud_and_occupancy_grid():
     # depth[seg == 11]
     # np.linspace(-15, 15)
 
-    # todo:
-    # 1. Plot x, y locations of all balls, and plot location of bla
-    # 2. Plot grid with linspace or other
-
     floor_height = 0.2001  # floor width was 0.2
     all_ball_x_y_locations_above_floor = [(ball_loc[0], ball_loc[1], ball_seg) for ball_loc, ball_seg in
                                           zip(all_ball_locations, all_ball_seg_objs)
                                           if ball_loc[2] > floor_height]
 
-    fig = plt.figure()
-    ax = fig.gca()
+    fig, (ax, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 10))
 
     ax.scatter([x[0] for x in all_ball_x_y_locations_above_floor],
                [x[1] for x in all_ball_x_y_locations_above_floor], s=1.5)
@@ -315,6 +325,7 @@ def create_point_cloud_and_occupancy_grid():
     ax.set_xticks(x_range)
     ax.set_yticks(y_range)
     ax.grid(True)
+    ax.set_title('Point Cloud 2D')
     plt.xticks(rotation=80)
     print('Time taken to create pointcloud and occupancy grid: {}'.format(
         time.time() - start_of_point_cloud_calculation))
@@ -327,57 +338,81 @@ def create_point_cloud_and_occupancy_grid():
         grid_x, grid_y = int((x + abs(x_range[0])) * 10), int((y + abs(y_range[0])) * 10)
         grid[grid.shape[0] - grid_y, grid_x] = 1
         grid_object_class[grid.shape[0] - grid_y, grid_x] = ball_seg_class
+        grid_list_object_class[grid.shape[0] - grid_y][grid_x].append(ball_seg_class)  # todo could double check if it properly works
 
     grid = np.zeros((y_range.shape[0], x_range.shape[0]))
     grid_object_class = np.zeros((y_range.shape[0], x_range.shape[0])) ## todo variable shape. todo do both for easy viz
-    grid_list_object_class = [[] * x_range.shape[0]]
+    grid_list_object_class = [[list() for x in range(x_range.shape[0])] for y in
+                              range(y_range.shape[0])]
 
     for ball in all_ball_x_y_locations_above_floor:
         set_closest_occupancy_grid(ball)
 
-    # import pdb;
-    # pdb.set_trace()
-    fig = plt.figure()
-    ax2 = fig.gca()
     fig.canvas.draw()
     ax2.set_xticks(range(x_range.shape[0]))
     ax2.set_yticks(range(y_range.shape[0]))
     ax2.set_xticklabels([round(x, 1) for x in x_range])
     ax2.set_yticklabels([round(y, 1) for y in y_range])
-    # ax2.grid(True)
     plt.xticks(rotation=80)
+    ax2.set_title('Occupancy Grid')
     ax2.imshow(grid, interpolation='none', cmap='gray')
 
-    fig = plt.figure()
-    ax3 = fig.gca()
     fig.canvas.draw()
     ax3.set_xticks(range(x_range.shape[0]))
     ax3.set_yticks(range(y_range.shape[0]))
     ax3.set_xticklabels([round(x, 1) for x in x_range])
     ax3.set_yticklabels([round(y, 1) for y in y_range])
+    ax3.set_title('Object Map')
     plt.xticks(rotation=80)
     num_unique_objects = len(np.unique(grid_object_class))
-    import pdb;pdb.set_trace()
+
     print('{} unique objects in segmentation buffer'.format(num_unique_objects))
-    mapping_to_positive_ints = {x: i for i, x in enumerate(np.unique(segBuffer))}
+    uniq_ints_in_seg_buffer = np.unique(segBuffer)
+    mapping_to_positive_ints = {x: i for i, x in enumerate(uniq_ints_in_seg_buffer)}
+    # mapping_from_positive_ints_to_orig = {i: x for i, x in enumerate(uniq_ints_in_seg_buffer)}
     # Make kitchen_walls_and_floor object the 0th index so tmp swap needed
     key_with_0_value = list(mapping_to_positive_ints.keys())[list(mapping_to_positive_ints.values()).index(0)]
-    mapping_to_positive_ints[key_with_0_value] = mapping_to_positive_ints[kitchen_walls_and_floor]
-    mapping_to_positive_ints[kitchen_walls_and_floor] = 0
+    mapping_to_positive_ints[key_with_0_value] = mapping_to_positive_ints[obj_name_to_obj_id['kitchen_walls_0']]
+    mapping_to_positive_ints[obj_name_to_obj_id['kitchen_walls_0']] = 0
 
+    grid_object_class = grid_object_class.astype('int64')
     grid_object_class_mapped_positive_ints = np.copy(grid_object_class)
     for key in mapping_to_positive_ints:
         grid_object_class_mapped_positive_ints[grid_object_class == key] = \
             mapping_to_positive_ints[key]
     ax3.imshow(grid_object_class_mapped_positive_ints, interpolation='none', cmap=new_cmap)
+    fontP = FontProperties()
+    fontP.set_size('small')
+    # reverse mapping. Colour should be the value of mapping_to_positive_ints[key]
+    # Label should be object name using original key
+    legend_elements = [Patch(facecolor=new_cmap(positive_val),
+                       label=obj_id_to_obj_name[key])
+                       for key, positive_val in mapping_to_positive_ints.items()
+                       if key in obj_id_to_obj_name]
+    box = ax3.get_position()
+    ax3.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax3.legend(loc='center left', handles=legend_elements, prop=fontP,  bbox_to_anchor=(1, 0.5))
 
-    # todo create terrain map?
+    print("obj_name_to_obj_id['kitchen_0']: {}".format(obj_name_to_obj_id['kitchen_0']))
+    print('mapping_to_positive_ints[obj_name_to_obj_id[\'kitchen_0\']: ', mapping_to_positive_ints[obj_name_to_obj_id['kitchen_0']])
+    positive_val = mapping_to_positive_ints[obj_name_to_obj_id['kitchen_0']]
+    c = new_cmap(positive_val)
 
+    print('new_cmap(positive_val): rgb({},{},{})'.format(int(c[0] * 255), int(c[1] * 255), int(c[2] * 255)))
+    plt.figure()
+    plt.imshow(segBuffer == obj_name_to_obj_id['kitchen_0'])
+    plt.figure()
+    arr = np.array([[0, 1, 2, 3, 4, 5, 7], [1, 1, 1, 1, 1, 1, 1]])
+    plt.imshow(arr, interpolation='none', cmap=new_cmap)
+
+    import pdb; pdb.set_trace()
     plt.show()
 
+
 # time.sleep(4)
-new_cmap = rand_cmap(100, type='bright', first_color_black=True, last_color_black=False)
+new_cmap = rand_cmap(100, type='bright', first_color_black=True, last_color_black=False, verbose=True)
 segLinkIndex = False
+print('Beginning physics simulation loop')
 for i in range(10000000):
     keys = p.getKeyboardEvents()
 
@@ -397,7 +432,7 @@ for i in range(10000000):
         flags = p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX
 
     p.stepSimulation()
-    # _, _, rgb, depth, seg = p.getCameraImage(320, 200, flags=flags)
+    # _, _, rgb, depth, seg = p.getCameraImage(320, 200, flags=flags)  # uncomment for updating every frame but slow
 
     # import pdb;pdb.set_trace()
     time.sleep(1/ 240)
